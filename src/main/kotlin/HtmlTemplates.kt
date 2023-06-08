b449e65ab4dd3ea13a06e0570947cbcc2277738a
OShane-McKenzie <oshanemckenzie5@gmail.com>
2023-06-08 13:04:41 -0500

    E-WEB

--- src/main/kotlin/HtmlTemplates.kt
+++ src/main/kotlin/HtmlTemplates.kt
@@ -3,27 +3,18 @@ fun hotel(){
     val html = buildString {
         append(Doctype())
         append(HtmlOpen())
-
-        append(HeadOpen())
-        append(TitleOpen())
-        append("Hotel Website")
-        append(TitleClose())
-        append(HeadClose())
-
+        append(HeadInline(TitleInline("Hotel Website")))
         append(BodyOpen())
-
         // Header Section
         append(
             HeaderInline(styles = Style(CSS.BACKGROUND_COLOR to "#333", CSS.PADDING to "20px"), contents = buildString {
             append(H1Inline(styles = Style(CSS.COLOR to "#fff"), contents = "Welcome to Our Hotel"))
         })
         )
-
         // Main Content Section
         append(SectionInline(styles = Style(CSS.PADDING to "20px"), contents = buildString {
             // Hero Image
             append(DivInline(styles = Style(CSS.BACKGROUND to "url('hero.png')", CSS.BACKGROUND_SIZE to "cover", CSS.HEIGHT to "400px"), contents = ""))
-
             // Hotel Information
             append(DivInline(styles = Style(CSS.PADDING to "20px"), contents = buildString {
                 append(H2Inline(styles = Style(CSS.COLOR to "#333"), contents = "About Our Hotel"))
@@ -33,16 +24,13 @@ fun hotel(){
                 append(ButtonInline(styles = Style(CSS.BACKGROUND_COLOR to "#333", CSS.COLOR to "#fff"), text = "Book Now", onClick = "bookHotel()"))
             }))
         }))
-
         // Footer Section
         append(FooterInline(styles = Style(CSS.BACKGROUND_COLOR to "#333", CSS.PADDING to "20px"), contents = buildString {
             append(PInline(styles = Style(CSS.COLOR to "#fff"), contents = "© 2023 Hotel. All rights reserved."))
         }))
-
         append(BodyClose())
-        append(HtmlInline(styles = Style(CSS.FONT_FAMILY to "Arial, sans-serif", CSS.COLOR to "#333"), contents = " ")) // Apply global styles
+        append(HtmlInline(styles = Style(CSS.FONT_FAMILY to "Arial, sans-serif", CSS.COLOR to "#333"), contents = " "))
     }
-
     saveHTML(html, "hotel")
 }
 
@@ -165,59 +153,56 @@ fun article(){
     val page = buildString{
         val headerStyle = Style(
             CSS.PADDING to "10px",
-            CSS.BACKGROUND_COLOR to black,
-            CSS.COLOR to white,
+            CSS.BACKGROUND_COLOR to cssBlack,
+            CSS.COLOR to cssWhite,
             CSS.TEXT_ALIGN to "center",
             CSS.FONT_SIZE to "32px"
         )
         val footerStyle = Style(
             CSS.PADDING to "10px",
-            CSS.BACKGROUND_COLOR to black,
-            CSS.COLOR to gold,
+            CSS.BACKGROUND_COLOR to cssBlack,
+            CSS.COLOR to cssGold,
             CSS.TEXT_ALIGN to "center",
             CSS.FONT_SIZE to "12px"
         )
         val articleTitleStyle = Style(
-            CSS.BACKGROUND_COLOR to gray,
+            CSS.BACKGROUND_COLOR to cssGray,
             CSS.TEXT_ALIGN to "center",
-            CSS.COLOR to white,
+            CSS.COLOR to cssWhite,
             CSS.MAX_WIDTH to "1000px",
             CSS.MARGIN to "0 auto",
 
         )
-
         val articleSubSectionsStyle = Style(
             CSS.FONT_WEIGHT to "bold",
             CSS.FONT_SIZE to "16px",
             CSS.MARGIN to "0 auto",
             CSS.MAX_WIDTH to "1000px",
-            CSS.COLOR to white
+            CSS.COLOR to cssWhite
         )
-
         val articleSubSectionsBodyStyle = Style(
             CSS.FONT_WEIGHT to "normal",
             CSS.FONT_SIZE to "16px",
             CSS.MAX_WIDTH to "1000px",
             CSS.MARGIN to "0 auto",
-            CSS.COLOR to white
+            CSS.COLOR to cssWhite
         )
-
         val bodyStyle = Style(
             CSS.BACKGROUND_IMAGE to "url('ftl.png')",
             CSS.BACKGROUND_REPEAT to "no-repeat",
             CSS.BACKGROUND_SIZE to "cover"
         )
-
         val title = TitleInline("ExampleBloggers.Com").nl()
         val docType = Doctype().nl()
         val header = HeaderInline("The Serious Blogger", headerStyle).nl()
         val footer = FooterInline("© 2023 Serious Blogger",footerStyle)
-
         val introduction = PInline("Introduction:",articleSubSectionsStyle).nl()
         val secondSection = PInline("Understanding the Cosmic Speed Limit:",articleSubSectionsStyle).nl()
         val thirdSection = PInline("Altering Space-Time:",articleSubSectionsStyle).nl()
-        val fourthSection = PInline("Quantum Entanglement and Quantum Tunnelling:",articleSubSectionsStyle).nl()
-        val fifthSection = PInline("The Path Ahead - Ongoing Research and Speculation:",articleSubSectionsStyle).nl()
+        val fourthSection = PInline("Quantum Entanglement and Quantum Tunnelling:",
+            articleSubSectionsStyle).nl()
+        val fifthSection = PInline("The Path Ahead - Ongoing Research and Speculation:",
+            articleSubSectionsStyle).nl()
         val sixthSection = PInline("Conclusion:",articleSubSectionsStyle).nl()
         val intro = PInline(blogIntro.lt(tabs = 3),articleSubSectionsBodyStyle).lt(lines = 2)
         val section2 = PInline(blogSection2.lt(tabs = 3),articleSubSectionsBodyStyle).lt(lines = 2)
@@ -225,41 +210,22 @@ fun article(){
         val section4 = PInline(blogSection4.lt(tabs = 3),articleSubSectionsBodyStyle).lt(lines = 2)
         val section5 = PInline(blogSection5.lt(tabs = 3),articleSubSectionsBodyStyle).lt(lines = 2)
         val section6 = PInline(blogSection6.lt(tabs = 3),articleSubSectionsBodyStyle).lt(lines = 2)
-
-        val blogTitle = DivInline(H2Inline("The Quest for Faster-Than-Light Travel: Breaking the Cosmic Speed Limit").nl(),articleTitleStyle).nl()
-
+        val blogTitle = DivInline(H2Inline("The Quest for Faster-Than-Light Travel: " +
+                "Breaking the Cosmic Speed Limit").nl(),articleTitleStyle).nl()
         val article = ArticleInline(
-            "\n"+blogTitle+
-                    introduction+
-                    intro+
-                    Br()+
-                    secondSection+
-                    section2+
-                    Br()+
-                    thirdSection+
-                    section3+
-                    Br()+
-                    fourthSection+
-                    section4+
-                    Br()+
-                    fifthSection+
-                    section5+
-                    Br()+
-                    sixthSection+
-                    section6,
-
+            "\n"+blogTitle+ introduction+ intro+ Br()+ secondSection+ section2+
+                    Br()+ thirdSection+ section3+ Br()+ fourthSection+ section4+ Br()+ fifthSection+ section5+ Br()+
+                    sixthSection+ section6,
             Style(
-                CSS.BACKGROUND_COLOR to transparentBlack70,
+                CSS.BACKGROUND_COLOR to cssTransparentBlack70,
                 CSS.MAX_WIDTH to "1050px",
                 CSS.MARGIN to "0 auto",
                 CSS.BORDER_RADIUS to "10px",
-                CSS.BORDER to "1px solid $white",
+                CSS.BORDER to "1px solid $cssWhite",
                 CSS.PADDING_BOTTOM to "10px"
-
             )
         ).nl()
         val body = BodyInline("\n"+article,bodyStyle).nl()
-
         append(docType,title,header,Br()+body+Br(),footer)
     }
     saveHTML(page ,"blog")
